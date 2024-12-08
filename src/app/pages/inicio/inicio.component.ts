41313ce832faa498eb18530972b28a5a97d28693
unknown <24yery05@gmail.com>
2024-12-08 15:30:38 -0400

    Descripción del cambio

---
+++ src/app/pages/inicio/inicio.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  standalone: false,
+  
+  templateUrl: './inicio.component.html',
+  styleUrl: './inicio.component.css'
+})
+export class InicioComponent {
+  nombre : string ='MiNombre ejemplo';
+
+}
